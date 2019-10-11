af007f86ec330c6a2a028d1f8de3c4cfff138885
cornway <romanpustobayev@gmail.com>
2019-10-11 08:03:40 +0300

    moving files

--- bin.c
+++
@@ -1,8 +0,0 @@
-#include <bin.h>
-
-int bsp_bin_reloc_abs (arch_word_t *progdata, arch_word_t offset,
-                            arch_word_t ldbegin, arch_word_t ldend)
-{
-    
-}
-
--- inc/bin.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BIN_H__
-#define __BIN_H__
-
-#include <bsp_sys.h>
-
-#endif /*__BIN_H__*/
