5b2f5523b13423ce54ac790e9d47126f72d02da7
margarin <romanpustobayev@gmail.com>
2019-11-12 20:08:12 +0200

    building stuff, initial commit

--- hal/serial.c
+++ hal/serial.c
@@ -52,55 +52,11 @@ static void serial_fatal (void)
 #error "Not supported"
 #endif
 
+#include "../../common/int/uart_nt.h"
+
 extern void serial_led_on (void);
 extern void serial_led_off (void);
 
-typedef enum {
-    SERIAL_DEBUG,
-    SERIAL_REGULAR,
-} serial_type_t;
-
-typedef struct uart_desc_s uart_desc_t;
-
-typedef void (*uart_msp_init_t) (uart_desc_t *uart_desc);
-typedef void (*uart_dma_init_t) (uart_desc_t *uart_desc);
-
-struct uart_desc_s {
-    USART_TypeDef           *hw;
-    UART_HandleTypeDef      handle;
-    UART_InitTypeDef  const * cfg;
-    uart_msp_init_t         msp_init;
-    uart_msp_init_t         msp_deinit;
-#if DEBUG_SERIAL_USE_DMA
-    uart_dma_init_t         dma_init;
-    uart_dma_init_t         dma_deinit;
-    DMA_HandleTypeDef       hdma_tx;
-    FlagStatus              uart_tx_ready;
-    IRQn_Type               irq_txdma, irq_uart;
-#if DEBUG_SERIAL_USE_RX
-    DMA_HandleTypeDef       hdma_rx;
-    IRQn_Type               irq_rxdma;
-    void                    (*rx_handler) (DMA_HandleTypeDef *);
-#endif
-#endif
-    serial_type_t           type;
-#if DEBUG_SERIAL_BUFERIZED
-    int                     active_stream;
-#endif
-    FlagStatus              initialized;
-};
-
-#if DEBUG_SERIAL_BUFERIZED
-
-#define STREAM_BUFSIZE 512
-#define STREAM_BUFCNT 2
-#define STREAM_BUFCNT_MS (STREAM_BUFCNT - 1)
-
-typedef struct {
-    char data[STREAM_BUFSIZE + 1];
-    int  bufposition;
-    uint32_t timestamp;
-} streambuf_t;
 
 static streambuf_t streambuf[STREAM_BUFCNT];
 
